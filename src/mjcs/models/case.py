5eceee97be810e7a3d287375d86b9157ef2c28aa
Dan Staples <dan@acab.enterprises>
2021-10-01 10:10:14 -0400

    Remove unused indices

--- src/mjcs/models/case.py
+++ src/mjcs/models/case.py
@@ -5,7 +5,6 @@ class Case(TableBase):
     __tablename__ = 'cases'
     __table_args__ = (
         Index('ixh_cases_case_number', 'case_number', postgresql_using='hash'),
-        Index('ixh_cases_detail_loc', 'detail_loc', postgresql_using='hash'),
     )
 
     case_number = Column(String, primary_key=True)
@@ -17,7 +16,7 @@ class Case(TableBase):
     status = Column(String, nullable=True, enum=True)
     caption = Column(String, nullable=True)
     loc = Column(Integer)
-    detail_loc = Column(String, index=True, enum=True)
-    last_scrape = Column(DateTime, nullable=True, index=True)
-    last_parse = Column(DateTime, nullable=True, index=True)
+    detail_loc = Column(String, enum=True)
+    last_scrape = Column(DateTime, nullable=True)
+    last_parse = Column(DateTime, nullable=True)
     active = Column(Boolean, nullable=False, server_default='true')
